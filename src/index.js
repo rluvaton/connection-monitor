05e32f8e4ca090dc3acb9bcc3cdf6396acb63cbc
Raz Luvaton <rluvaton@gmail.com>
2020-04-23 17:53:14 +0300

    Fix (a patch for now) that the error column not long enough set the test script in the package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const {run} = require('./run');
 
 run([
-    {name: 'Router', ip: '192.168.1.1', isAlive: false},
-    {name: 'Home Server', ip: '192.168.1.16', isAlive: false, comment: 'Ubuntu'},
-    {name: 'Local', ip: '127.0.0.1', isAlive: false, comment: 'Local Host'},
+    {name: 'Router', ip: '192.168.1.1', isAlive: false, error: '                                                      '},
+    {name: 'Home Server', ip: '192.168.1.16', isAlive: false, comment: 'Ubuntu', error: '                                                      '},
+    {name: 'Local', ip: '127.0.0.1', isAlive: false, comment: 'Local Host', error: '                                                      '},
 ]);
